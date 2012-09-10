122a4a9496f3e5f910311cb5565877811a1fbeeb
Wingston Sharon <wingston.sharon@gmail.com>
2012-09-10 22:14:49 +0530

    got the selected item list fragment up and running.. :D kinda convoluted.. next commit comments come!

--- src/com/example/testtab/DetailFrag.java
+++ src/com/example/testtab/DetailFrag.java
@@ -10,16 +10,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
+import android.widget.ListView;
 
 
 public class DetailFrag extends Fragment {
 	public Button button_back;
-	public CheckBox cb;
+	public ListView lv;
 	ArrayList<BaseItem> myitemlist;
 	public int n=0;
 	public String strng="default";
+	private ArrayAdapter arrayAdapter;
 	
 	DetailFrag(ArrayList<BaseItem> s)
 	{
@@ -29,13 +31,21 @@ public class DetailFrag extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {	
         View view = inflater.inflate(R.layout.detail, container, false);
         button_back =(Button) view.findViewById(R.id.button1);
-        cb = (CheckBox)view.findViewById(R.id.checkBox1);
+        lv = (ListView)view.findViewById(R.id.listview1);
      
      //   cb.setText(myitemlist.get(0).title);
+        String[] arr = new String[myitemlist.size()];//myitemlist.toArray(new String[0]);
         for(int i=0;i<myitemlist.size();i++)
+        {	
         	Log.d("XXX",Integer.toString(i)+myitemlist.get(i).title);
+        	arr[i]=myitemlist.get(i).title;
+        }
         
-      //  Log.d("XXX","*/*"+str);fd
+        
+        arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, arr);
+
+        lv.setAdapter(arrayAdapter);
+        
         
         button_back.setOnClickListener(new OnClickListener() {
         
